2ae05d75d018fe817f8a1493c61c6963e9564c33
sruthianem89 <sruthianem89@gmail.com>
2024-08-07 12:48:18 -0400

    terraform changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
 	bucket         = "terraformstatesru"
 	key            = "path/to/your/terraform.tfstate"
-	region         = var.region_name
+	region         = "us-east-1"
   }
 }
 
