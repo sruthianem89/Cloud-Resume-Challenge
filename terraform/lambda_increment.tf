6ee9994db7628b12853677ec54c57ea3749d2785
sruthianem89 <sruthianem89@gmail.com>
2024-08-08 08:15:28 -0400

    Removed lambda increment duplicates

--- terraform/lambda_increment.tf
+++ terraform/lambda_increment.tf
@@ -18,19 +18,9 @@ resource "aws_lambda_function" "frontend_lambda" {
   runtime       = var.lambda_runtime
   role          = aws_iam_role.lambda_role.arn
 
-  # Pass the DynamoDB table name and Lambda function URL as environment variables
-  environment {
-    variables = {
-      DYNAMODB_TABLE_NAME = aws_dynamodb_table.visitor_count_table.name
-    }
-  }
-
-  # Ensure the Lambda function is created after the zip file and the initialize_lambda function
+  # Ensure the Lambda function is created after the zip file
   depends_on = [
-	null_resource.zip_lambda_function_code,
-	aws_dynamodb_table.visitor_count_table,
-	aws_lambda_function.initialize_lambda,
-	aws_lambda_function_url.initialize_lambda_url
+	null_resource.zip_lambda_function_code
   ]
 }
 
@@ -45,55 +35,4 @@ resource "aws_lambda_function_url" "frontend_lambda_url" {
 
   # Ensure the function URL is created after the frontend_lambda function
   depends_on = [aws_lambda_function.frontend_lambda]
-}
-
-# Define the DynamoDB table
-resource "aws_dynamodb_table" "visitor_count_table" {
-  name           = "VisitorCountTable"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "ID"
-
-  attribute {
-	name = "ID"
-	type = "S"
-  }
-}
-
-# Define the Lambda function for initialization (example)
-resource "aws_lambda_function" "initialize_lambda" {
-  function_name = "initialize_lambda"
-  filename      = "${path.module}/../initialize_lambda_function.zip"
-  handler       = "initialize_lambda_function.lambda_handler"
-  runtime       = var.lambda_runtime
-  role          = aws_iam_role.lambda_role.arn
-
-  # Ensure the Lambda function is created after the zip file
-  depends_on = [
-	null_resource.zip_initialize_lambda_function_code
-  ]
-}
-
-# Zip Lambda function code for initialization (example)
-resource "null_resource" "zip_initialize_lambda_function_code" {
-  provisioner "local-exec" {
-	command = "cd ../backend && zip -r ../initialize_lambda_function.zip initialize_lambda_function.py"
-  }
-
-  # Ensure the zip is created before the Lambda function
-  triggers = {
-	always_run = "${timestamp()}"
-  }
-}
-
-# Enable function URL and CORS for initialization Lambda (example)
-resource "aws_lambda_function_url" "initialize_lambda_url" {
-  function_name      = aws_lambda_function.initialize_lambda.function_name
-  authorization_type = "NONE"
-
-  cors {
-	allow_origins = ["*"]
-  }
-
-  # Ensure the function URL is created after the initialize_lambda function
-  depends_on = [aws_lambda_function.initialize_lambda]
 }
