a25bb6ba8aba1222e40218fd53f69441c558f14b
sruthianem89 <sruthianem89@gmail.com>
2024-08-09 10:51:55 -0400

    EM #35

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -9,56 +9,57 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
-	// Activate Bootstrap scrollspy on the main nav element
-	const sideNav = document.body.querySelector('#sideNav');
-	if (sideNav) {
-		new bootstrap.ScrollSpy(document.body, {
-			target: '#sideNav',
-			rootMargin: '0px 0px -40%',
-		});
-	};
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    }
 
-	// Collapse responsive navbar when toggler is visible
-	const navbarToggler = document.body.querySelector('.navbar-toggler');
-	const responsiveNavItems = [].slice.call(
-		document.querySelectorAll('#navbarResponsive .nav-link')
-	);
-	responsiveNavItems.map(function (responsiveNavItem) {
-		responsiveNavItem.addEventListener('click', () => {
-			if (window.getComputedStyle(navbarToggler).display !== 'none') {
-				navbarToggler.click();
-			}
-		});
-	});
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
 
-	// Initialize DynamoDB table by calling Lambda Function URL with the table name
-	fetch('INITIALIZE_FUNCTION_URL', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({ tableName: "DYNAMODB_TABLE_NAME" })
-	})
-	.finally(() => {
-		// Fetch count value from Lambda Function and update the page
-		fetch('LAMBDA_FUNCTION_URL', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ tableName: "DYNAMODB_TABLE_NAME" })
-		})
-	.then(response => {
-		if (!response.ok) {
-			throw new Error('Network response was not ok');
-		}
-		return response.text();
-	})
-	.then(data => {
-		document.getElementById('count-value').textContent = data;
-	})
-	.catch(error => {
-		console.error('There was a problem with the fetch operation:', error);
-		document.getElementById('count-value').textContent = 'Error loading count value';
-	});
+    // Initialize DynamoDB table by calling Lambda Function URL with the table name
+    fetch('INITIALIZE_FUNCTION_URL', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ tableName: "DYNAMODB_TABLE_NAME" })
+    })
+    .finally(() => {
+        // Fetch count value from Lambda Function and update the page
+        fetch('LAMBDA_FUNCTION_URL', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ tableName: "DYNAMODB_TABLE_NAME" })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('count-value').textContent = data;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+            document.getElementById('count-value').textContent = 'Error loading count value';
+        });
+    });
 });
