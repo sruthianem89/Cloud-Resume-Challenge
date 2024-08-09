68b11e7005b4050dccffa6f2256ccb997eec2e5b
sruthianem89 <sruthianem89@gmail.com>
2024-08-09 08:29:11 -0400

    EM #25

--- terraform/lambda_initialize_dynamodb.tf
+++ terraform/lambda_initialize_dynamodb.tf
@@ -62,7 +62,7 @@ resource "aws_lambda_function_url" "initialize_lambda_url" {
 
   cors {
 	allow_origins = ["*"]
-  allow_methods = ["GET", "POST", "OPTIONS"] 
+  allow_methods = ["*"] 
   }
 
   # Ensure the function URL is created after the Lambda function
