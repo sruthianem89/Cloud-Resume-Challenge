87065e748cd36918f648dceb973038ac533b4975
sruthianem89 <sruthianem89@gmail.com>
2024-08-09 07:33:09 -0400

    EM 22#

--- backend/initialize_dynamodb.py
+++ backend/initialize_dynamodb.py
@@ -4,7 +4,7 @@ import boto3 # type: ignore
 dynamodb = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
-    # Initialize response headers for CORS
+    # Initialize response headers for CORS:
     headers = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
