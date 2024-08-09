5e6bb880a3adfccd2009f31f94cafc8aadb9f3ab
sruthianem89 <sruthianem89@gmail.com>
2024-08-09 15:42:42 -0400

    EM 36#

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -39,27 +39,34 @@ window.addEventListener('DOMContentLoaded', event => {
         },
         body: JSON.stringify({ tableName: "DYNAMODB_TABLE_NAME" })
     })
-    .finally(() => {
+    .then(response => {
+        if (!response.ok) {
+            return response.text().then(text => { throw new Error(text) });
+        }
+        return response.text();
+    })
+    .then(data => {
+        console.log('DynamoDB initialization response:', data);
         // Fetch count value from Lambda Function and update the page
-        fetch('LAMBDA_FUNCTION_URL', {
+        return fetch('LAMBDA_FUNCTION_URL', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({ tableName: "DYNAMODB_TABLE_NAME" })
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => {
-            document.getElementById('count-value').textContent = data;
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-            document.getElementById('count-value').textContent = 'Error loading count value';
         });
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('count-value').textContent = data;
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        document.getElementById('count-value').textContent = 'Error loading count value';
     });
 });
