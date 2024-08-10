75ee04012a1c259567d92333c2b6c0ca8e4530dd
sruthianem89 <sruthianem89@gmail.com>
2024-08-09 23:32:30 -0400

    flare2

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,11 +1,13 @@
-#Depends on Cloudfront Distribution creation
-
 resource "cloudflare_record" "root_domain" {
   zone_id = data.cloudflare_zones.zone.zones[0].id
   name    = "sruthianem.com"
   value   = aws_cloudfront_distribution.frontend_distribution.domain_name
   type    = "CNAME"
   proxied = true
+
+  depends_on = [
+    aws_cloudfront_distribution.frontend_distribution
+  ]
 }
 
 resource "cloudflare_record" "wildcard_subdomain" {
@@ -14,4 +16,8 @@ resource "cloudflare_record" "wildcard_subdomain" {
   value   = aws_cloudfront_distribution.frontend_distribution.domain_name
   type    = "CNAME"
   proxied = true
+
+  depends_on = [
+    aws_cloudfront_distribution.frontend_distribution
+  ]
 }
