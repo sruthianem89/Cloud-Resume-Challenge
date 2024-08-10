f9383b7b1c6a5fbe044aba431dd9d10bcf6b454a
sruthianem89 <sruthianem89@gmail.com>
2024-08-10 14:20:29 -0400

    afterEach hook

--- cypress/e2e/DOM.cy.js
+++ cypress/e2e/DOM.cy.js
@@ -4,18 +4,7 @@ describe('Web Tests', () => {
   const resumeUrl = 'WEBSITE_URL'; // Replace with your actual resume URL
   const resetUrl = 'RESET_FUNCTION_URL'; // Replace with your actual reset URL
 
-  it('should display the visitor count in the DOM and not show an error message', () => {
-    // Visit the resume page
-    cy.visit(resumeUrl);
-
-    // Wait for the visitor count to be updated and ensure it's not an error message
-    cy.get('#count-value', { timeout: 20000 }) // Adjust the timeout if needed
-      .should(($countValue) => {
-        // Ensure that the count value is not an error message
-        const text = $countValue.text().trim();
-        expect(text).to.not.equal('Error loading count value');
-      });
-
+  afterEach(() => {
     // Call the reset URL with the DynamoDB table name in the body to reset the counter
     cy.request({
       method: 'POST',
@@ -29,5 +18,18 @@ describe('Web Tests', () => {
     }).then((response) => {
       expect(response.status).to.equal(200);
     });
+  });
+
+  it('should display the visitor count in the DOM and not show an error message', () => {
+    // Visit the resume page
+    cy.visit(resumeUrl);
+
+    // Wait for the visitor count to be updated and ensure it's not an error message
+    cy.get('#count-value', { timeout: 20000 }) // Adjust the timeout if needed
+      .should(($countValue) => {
+        // Ensure that the count value is not an error message
+        const text = $countValue.text().trim();
+        expect(text).to.not.equal('Error loading count value');
+      });
   });
 });
