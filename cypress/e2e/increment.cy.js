158ded4a40b3774ef859210e8ec22106b7aa53ac
sruthianem89 <sruthianem89@gmail.com>
2024-08-10 00:09:02 -0400

    cypress4

--- cypress/e2e/increment.cy.js
+++ cypress/e2e/increment.cy.js
@@ -1,16 +1,35 @@
 describe('API Tests', () => {
-    const apiUrl = 'LAMBDA_FUNCTION_URL';
-  
-    it('should update the database', () => {
-      let initialCounter;
-      
-      cy.request(apiUrl).then((response) => {
-        initialCounter = parseInt(response.body, 10);
-        return cy.request(apiUrl);
-      }).then((response) => {
-        const updatedCounter = parseInt(response.body, 10);
-        expect(updatedCounter).to.eq(initialCounter + 1);
+  const apiUrl = 'LAMBDA_FUNCTION_URL';
+
+  it('should update the database', () => {
+    let initialCounter;
+
+    // Initial GET request to fetch the current counter value
+    cy.request(apiUrl).then((response) => {
+      initialCounter = parseInt(response.body, 10);
+
+      // POST request to increment the counter
+      return cy.request({
+        method: 'POST',
+        url: apiUrl,
+        body: JSON.stringify({ tableName: "DYNAMODB_TABLE_NAME" }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
       });
+    }).then(() => {
+      // GET request to fetch the updated counter value
+      return cy.request({
+        method: 'POST',
+        url: apiUrl,
+        body: JSON.stringify({ tableName: "DYNAMODB_TABLE_NAME" }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    }).then((response) => {
+      const updatedCounter = parseInt(response.body, 10);
+      expect(updatedCounter).to.eq(initialCounter + 1);
     });
   });
-  
+});
--- cypress/e2e/initialize.cy.js
+++ cypress/e2e/initialize.cy.js
@@ -1,14 +1,20 @@
 describe('API Tests', () => {
-    const apiUrl = 'INITIALIZE_FUNCTION_URL';
-  
-    it('should return correct status code and body', () => {
-      cy.request(apiUrl).then((response) => {
-        // Check the status code
-        expect(response.status).to.eq(200);
-  
-        // Optionally log the response body for debugging
-        console.log(response.body);
-      });
+  const apiUrl = 'INITIALIZE_FUNCTION_URL';
+
+  it('should return correct status code and body', () => {
+    cy.request({
+      method: 'POST',
+      url: apiUrl,
+      body: JSON.stringify({ tableName: "DYNAMODB_TABLE_NAME" }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      // Check the status code
+      expect(response.status).to.eq(200);
+
+      // Optionally log the response body for debugging
+      console.log(response.body);
     });
   });
-  
+});
